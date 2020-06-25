c701b564ec4517051b8012e049a2aa39517d7875
Master_Boy <osorio.alvarez.adrian@gmail.com>
2020-06-24 23:19:55 -0500

    Add: en la clase computer se agregan metodos get Add: en la clase hamburger se agregan metodos set y get y metodo preparingHamburger

--- java-course/src/main/java/com/examples/Computer.java
+++ java-course/src/main/java/com/examples/Computer.java
@@ -8,13 +8,13 @@ package com.examples;
 //[accessModifier] class [className]
 public class Computer {
 
-	//1.Fields (variables)
-	//Describen las propiedades de una cosa.
-	//[accessModifier] [dataType] [name]
+	// 1.Fields (variables)
+	// Describen las propiedades de una cosa.
+	// [accessModifier] [dataType] [name]
 	private String marca;
-	
-	//TODO: Agregar 10 campos mas con su respectivos set y get
-	
+
+	// TODO: Agregar 10 campos mas con su respectivos set y get
+
 	private String modelo;
 	private String color;
 	private String procesador;
@@ -24,47 +24,85 @@ public class Computer {
 	private String tarjetaMadre;
 	private String tarjetaAudio;
 	private String tarjetaVideo;
-	
-	
-	//2.Constructor
-	//Sirve para inicializar los campos (variables) de tu clase
-	//[accessModifier] [same className]
+
+	// 2.Constructor
+	// Sirve para inicializar los campos (variables) de tu clase
+	// [accessModifier] [same className]
 	public Computer() {
-		
 	}
-	
-	//3.Methods
-	//[accessModifier] [returnType] [methodName] ( parameterList... )
-	public void setMarca( String marca ) {
+
+	// 3.Methods
+	// [accessModifier] [returnType] [methodName] ( parameterList... )
+	public void setMarca(String marca) {
 		this.marca = marca;
 	}
-	
-	public void setColor( String color ) {
+
+	public void setColor(String color) {
 		this.color = color;
 	}
-	public void setProcesador( String procesador ) {
+
+	public void setProcesador(String procesador) {
 		this.procesador = procesador;
 	}
-	public void setMemoria( String memoria ) {
+
+	public void setMemoria(String memoria) {
 		this.memoria = memoria;
 	}
-	public void setDiscoDuro( String discoDuro ) {
+
+	public void setDiscoDuro(String discoDuro) {
 		this.discoDuro = discoDuro;
 	}
-	public void setTamanioPantalla( String tamanioPantalla ) {
+
+	public void setTamanioPantalla(String tamanioPantalla) {
 		this.tamanioPantalla = tamanioPantalla;
 	}
-	public void setTarjetaMadre( String tarjetaMadre ) {
+
+	public void setTarjetaMadre(String tarjetaMadre) {
 		this.tarjetaMadre = tarjetaMadre;
 	}
-	public void setTarjetaAudio( String tarjetaAudio ) {
+
+	public void setTarjetaAudio(String tarjetaAudio) {
 		this.tarjetaAudio = tarjetaAudio;
 	}
-	public void setTarjetaVideo( String tarjetaVideo ) {
+
+	public void setTarjetaVideo(String tarjetaVideo) {
 		this.tarjetaVideo = tarjetaVideo;
 	}
-	//TOOD: Agrega un comentario con las partes del metodo
+
+	// TOOD: Agrega un comentario con las partes del metodo
 	public String getMarca() {
 		return marca;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public String getProcesador() {
+		return procesador;
+	}
+
+	public String getMemoria() {
+		return memoria;
+	}
+
+	public String getDiscoDuro() {
+		return discoDuro;
+	}
+
+	public String getTamanioPantalla() {
+		return tamanioPantalla;
+	}
+
+	public String getTarjetaMadre() {
+		return tarjetaMadre;
+	}
+
+	public String getTarjetaAudio() {
+		return tarjetaAudio;
+	}
+
+	public String getTarjetaVideo() {
+		return tarjetaVideo;
 	}
 }
--- java-course/src/main/java/com/examples/Hamburger.java
+++ java-course/src/main/java/com/examples/Hamburger.java
@@ -3,33 +3,90 @@ package com.examples;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Hamburger{
-	
-	//Campos, variables o propiedades
-	private List<String> ingredients = new ArrayList<>(); 
+public class Hamburger {
+
+	// Campos, variables o propiedades
+	private List<String> ingredients = new ArrayList<>();
 	private double precio;
-	
-	
-	//Constructor default
+	private String tamanio;
+	private String tipoCarne;
+	private String terminoCarne;
+	private String tipoPan;
+	private String nombreHamburguesa;
+
+	// Constructor default
 	public Hamburger() {
 	}
-	
-	public Hamburger( List<String> ingredients, double precio ) {
+
+	public Hamburger(List<String> ingredients, double precio) {
 		this.ingredients = ingredients;
 		this.precio = precio;
 	}
-	
-	//Methodo
-	//TODO: @Adrian: Agregar los metodos set y get que hagan falta
-	
-	//TODO: Agregar un metodo que se llama prepareHamburger
-	
-	public void packageHamburger() {
-		System.out.println( "Recibiendo hamburguesa..." );
-		System.out.println( "Empapelando hamburguesa..." );
-		System.out.println( "Empaquetanto hamburguesa..." );
-		
-		System.out.println( "Lista para entregar" );
+
+	// Methodo
+	// TODO: @Adrian: Agregar los metodos set y get que hagan falta
+
+	// Metodos set
+
+	public void setTamanio(String tamanio) {
+		this.tamanio = tamanio;
 	}
-	
+
+	public void setTipoCarne(String tipoCarne) {
+		this.terminoCarne = tamanioCarne;
+	}
+
+	public void setTerminoCarne(String terminoCarne) {
+		this.terminoCarne = terminoCarne;
+	}
+
+	public void setTipoPan(String tipoPan) {
+		this.tipoPan = tipoPan;
+	}
+
+	public void setNombreHamburguesa(String nombreHamburguesa) {
+		this.nombreHamburguesa = nombreHamburguesa;
+	}
+
+	// Metodos get
+
+	public String getTamanio() {
+		return tamanio;
+	}
+
+	public String getTipoCarne() {
+		return tipoCarne;
+	}
+
+	public String getTerminoCarne() {
+		return terminoCarne;
+	}
+
+	public String getTipoPan() {
+		return tipoPan;
+	}
+
+	public String getNombreHamburguesa() {
+		return nombreHamburguesa;
+	}
+
+	// TODO: Agregar un metodo que se llama prepareHamburger
+
+	public void preparingHamburger() {
+		System.out.println("Cocinando carne...");
+		System.out.println("Calentando pan...");
+		System.out.println("Zazonando ingredientes...");
+		System.out.println("Armando hamburguesa...");
+
+		System.out.println("Lista para empacar...");
+	}
+
+	public void packageHamburger() {
+		System.out.println("Recibiendo hamburguesa...");
+		System.out.println("Empapelando hamburguesa...");
+		System.out.println("Empaquetanto hamburguesa...");
+
+		System.out.println("Lista para entregar");
+	}
+
 }

ed95037c2c77fa734b827f1ab5abefad0fec3280
Master_Boy <osorio.alvarez.adrian@gmail.com>
2020-06-25 20:03:54 -0500

    Add: metodos set y get faltantes y main

--- java-course/src/main/java/com/examples/Hamburger.java
+++ java-course/src/main/java/com/examples/Hamburger.java
@@ -6,19 +6,12 @@ import java.util.List;
 public class Hamburger {
 
 	/*
-	 * Datos primitivos y su valor por default
-	 * byte = 0
-	 * short = 0
-	 * boolean = false
-	 * int = 0
-	 * long = 0
-	 * double = 0.0
-	 * float = 0.0
-	 * char = ''
+	 * Datos primitivos y su valor por default byte = 0 short = 0 boolean = false
+	 * int = 0 long = 0 double = 0.0 float = 0.0 char = ''
 	 */
-	
+
 	//
-	//Campos de instancia
+	// Campos de instancia
 	private List<String> ingredients = new ArrayList<>();
 	private double precio = 0.0;
 	private String tamanio;
@@ -30,12 +23,14 @@ public class Hamburger {
 
 	// Constructor default
 	public Hamburger() {
-		//TODO: @Adrian pon un mensaje imprimiendo todas las propiedades o campos y sus valores
+		// TODO: @Adrian pon un mensaje imprimiendo todas las propiedades o campos y sus
+		// valores
 	}
 
 	public Hamburger(List<String> ingredients, double precio) {
-		//TODO: @Adrian complementa este contrcutor agregando n parametros segun la cantidad de campos que tengas
-		//y tambien muestra un mensaje 
+		// TODO: @Adrian complementa este contrcutor agregando n parametros segun la
+		// cantidad de campos que tengas
+		// y tambien muestra un mensaje
 		this.ingredients = ingredients;
 		this.precio = precio;
 	}
@@ -44,6 +39,14 @@ public class Hamburger {
 	// TODO: @Adrian: Agregar los metodos set y get que hagan falta
 
 	// Metodos set
+
+	public void setIngredients(List<String> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
 
 	public void setTamanio(String tamanio) {
 		this.tamanio = tamanio;
@@ -65,7 +68,19 @@ public class Hamburger {
 		this.nombreHamburguesa = nombreHamburguesa;
 	}
 
+	public char getRanking() {
+		return ranking;
+	}
+
 	// Metodos get
+
+	public List<String> getIngredients() {
+		return ingredients;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
 
 	public String getTamanio() {
 		return tamanio;
@@ -87,6 +102,10 @@ public class Hamburger {
 		return nombreHamburguesa;
 	}
 
+	public void setRanking(char ranking) {
+		this.ranking = ranking;
+	}
+
 	// TODO: Agregar un metodo que se llama prepareHamburger
 
 	public void preparingHamburger() {
@@ -105,5 +124,4 @@ public class Hamburger {
 
 		System.out.println("Lista para entregar");
 	}
-
 }
--- java-course/src/main/java/com/examples/HamburgerMain.java
+++ java-course/src/main/java/com/examples/HamburgerMain.java
@@ -1,28 +1,44 @@
 package com.examples;
 
 public class HamburgerMain {
-	
+
 	/*
 	 * El metodo main sirve como puerta de entrada para que la maquina virtual
 	 * ejecute nuestro programa
 	 */
-	
-	//Forma 1
+
+	// Forma 1
 //	public static void main( String[] args ) {
 //		
 //	}
-	
-	//Forma 2
+
+	// Forma 2
 //	public static void main( String args[] ) {
 //		
 //	}
-	
-	//Forma 3
-	public static void main( String... args ) {
+
+	// Forma 3
+	public static void main(String... args) {
 		Hamburger h1 = new Hamburger();
 		h1.setNombreHamburguesa("Doble carne");
-		
+		h1.setTipoPan("Pan de ajo");
+		h1.setTipoCarne("Arrachera marinada");
+		h1.setTamanio("Mediana");
+		h1.setPrecio(75.50);
+		h1.setTerminoCarne("En su punto");
+
 		Hamburger h2 = new Hamburger();
+		h2.setNombreHamburguesa("Mexicana");
+		h2.setTipoPan("Pan de chile");
+		h2.setTipoCarne("Carne molida con especies");
+		h2.setTamanio("Grande");
+		h2.setPrecio(98.50);
+		h2.setTerminoCarne("En su punto");
+
+		System.out.println("Hamburguesa: " + h1.getNombreHamburguesa());
+		h1.packageHamburger();
 		
+		System.out.println("\n" + "Hamburguesa: " + h2.getNombreHamburguesa());
+		h1.preparingHamburger();
 	}
 }

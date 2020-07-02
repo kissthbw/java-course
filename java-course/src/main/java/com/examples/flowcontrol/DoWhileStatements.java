2f1adf77d6bab4db74ceae41bd40db5ef39f2d21
Juan Osorio <kissthbw@gmail.com>
2020-07-02 12:32:24 -0500

    Add: Clase 2 de julio 2020 (while, do while parte 2)

--- java-course/src/main/java/com/examples/flowcontrol/DoWhileStatements.java
+++ java-course/src/main/java/com/examples/flowcontrol/DoWhileStatements.java
@@ -5,8 +5,24 @@ public class DoWhileStatements {
 	// Este metodo va encontrar el numero pasado parametro dentro de un arreglo de
 	// enteros
 	public void encuentraNumero(int[] array, int numero) {
-
+		int i = 0;
+		while( i < array.length ) {
+			System.out.println( array[ i ] );
+			
+			if( numero == array[ i ] ) {
+				System.out.println( "El numero: " + numero + 
+						" esta en la posicion: " + i );
+				
+				break; //Sirve para salirte del bicle de ejecucion
+			}
+			
+			i++;
+		}
 	}
+	
+	//TODO: Adrian haz otro metodo encuentraNumero2, pero en lugar de usar while, usa do while
+	//TODO: Adrian haz otro metodo encuentraOcuurencias, que reciba un array y un numero a buscar
+	//imprimir las n veces que se encuentre numero en el array
 
 	public static void main(String[] args) {
 		// while(){}
@@ -30,17 +46,9 @@ public class DoWhileStatements {
 
 		DoWhileStatements ejemplo = new DoWhileStatements();
 		int[] array = { 35, 36, 14, 37 };
-		int numero = 10;
-		i = 0;
+		int numero = 14;
+		
+		//Los arreglos iniciian en la posicion 0 
 		ejemplo.encuentraNumero(array, numero);
-
-		// se recorre el arreglo con un for para evaluar dentro de la estructura if, si
-		// el numero buscado existe dentro del arreglo, si es asi imprime el numero y su
-		// posicion
-		for (i = 0; i <= array.length; i++) {
-			if (numero == array[i]) {
-				System.out.println("el numero: " + numero + " esta en la posicion: " + i);
-			}
-		}
 	}
 }

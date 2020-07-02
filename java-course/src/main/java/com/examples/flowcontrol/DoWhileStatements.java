7a72ca47ffec71ba80111929c05df827e58a2e3c
Master_Boy <osorio.alvarez.adrian@gmail.com>
2020-07-02 11:46:47 -0500

    Add: ejercicio encontrar numero

--- java-course/src/main/java/com/examples/flowcontrol/DoWhileStatements.java
+++ java-course/src/main/java/com/examples/flowcontrol/DoWhileStatements.java
@@ -2,39 +2,45 @@ package com.examples.flowcontrol;
 
 public class DoWhileStatements {
 
-	//Este metodo va encontrar el numero pasado parametro dentro de unarreglo de enteros
-	public void encuentraNumero( int[] array, int numero ) {
-		
+	// Este metodo va encontrar el numero pasado parametro dentro de un arreglo de
+	// enteros
+	public void encuentraNumero(int[] array, int numero) {
+
 	}
-	
-	public static void main( String[] args ) {
-		//while(){}
-		//Evalua una expresion boolean y ejecuta el codigo ubicado entre {}
-		//hasta que la expresion sea false
-		
-		System.out.println( "While..." );
+
+	public static void main(String[] args) {
+		// while(){}
+		// Evalua una expresion boolean y ejecuta el codigo ubicado entre {}
+		// hasta que la expresion sea false
+
+		System.out.println("While...");
 		int i = 0;
-		while( i <= 10 ) {
-			System.out.println( i );
-//			i++;//post increment
+		while (i <= 10) {
+			System.out.println(i);
+			// i++;//post increment
 			i = i + 1;
 		}
-		
-		System.out.println( "Do While..." );
+
+		System.out.println("Do While...");
 		i = 11;
 		do {
-			System.out.println( i );
+			System.out.println(i);
 			i++;
-		}while( i <= 10 );
-		
+		} while (i <= 10);
+
 		DoWhileStatements ejemplo = new DoWhileStatements();
 		int[] array = { 35, 36, 14, 37 };
-		int numero = 14;
-		ejemplo.encuentraNumero( array, numero );
-		
-		
+		int numero = 10;
+		i = 0;
+		ejemplo.encuentraNumero(array, numero);
+
+		// se recorre el arreglo con un for para evaluar dentro de la estructura if, si
+		// el numero buscado existe dentro del arreglo, si es asi imprime el numero y su
+		// posicion
+		for (i = 0; i <= array.length; i++) {
+			if (numero == array[i]) {
+				System.out.println("el numero: " + numero + " esta en la posicion: " + i);
+			}
+		}
 	}
-	
-	
-	
 }

44fd738a256c2b8db3db07b2074af031c05c3001
Master_Boy <osorio.alvarez.adrian@gmail.com>
2020-07-03 10:52:11 -0500

    Add: ejercicios encuentraNumero2 y encontrarOcurrencias

--- java-course/src/main/java/com/examples/flowcontrol/DoWhileStatements.java
+++ java-course/src/main/java/com/examples/flowcontrol/DoWhileStatements.java
@@ -6,23 +6,49 @@ public class DoWhileStatements {
 	// enteros
 	public void encuentraNumero(int[] array, int numero) {
 		int i = 0;
-		while( i < array.length ) {
-			System.out.println( array[ i ] );
+		while (i < array.length) {
+			// System.out.println( array[ i ] );
+
+			if (numero == array[i]) {
+				System.out.println("El numero: " + numero + " esta en la posicion: " + i);
+
+				//break; // Sirve para salirte del bicle de ejecucion
+			}
+
+			i++;
+		}
+	}
+
+	// TODO: Adrian haz otro metodo encuentraNumero2, pero en lugar de usar while,
+	// usa do while
+
+	public void encuentraNumero2(int[] array, int numero) {
+		int i = 0;
+		do {
+			if (numero == array[i]) {
+				System.out.println("El numero: " + numero + " esta en la posicion: " + i);
+			}
 			
-			if( numero == array[ i ] ) {
-				System.out.println( "El numero: " + numero + 
-						" esta en la posicion: " + i );
-				
-				break; //Sirve para salirte del bicle de ejecucion
+			i++;
+		} while (i < array.length) ;
+	}
+
+	// TODO: Adrian haz otro metodo encuentraOcuurencias, que reciba un array y un
+	// numero a buscar
+	// imprimir las n veces que se encuentre numero en el array
+	
+	public void encuentraOcurrencias(int[] array, int numero) {
+		int i= 0;
+		int contador = 0;
+		while(i < array.length) {
+			if (numero == array[i]) {
+				contador ++;
+				System.out.println("El numero: " + numero + " esta: " + contador + " vez(ces)");
 			}
 			
 			i++;
 		}
 	}
-	
-	//TODO: Adrian haz otro metodo encuentraNumero2, pero en lugar de usar while, usa do while
-	//TODO: Adrian haz otro metodo encuentraOcuurencias, que reciba un array y un numero a buscar
-	//imprimir las n veces que se encuentre numero en el array
 
 	public static void main(String[] args) {
 		// while(){}
@@ -45,10 +71,12 @@ public class DoWhileStatements {
 		} while (i <= 10);
 
 		DoWhileStatements ejemplo = new DoWhileStatements();
-		int[] array = { 35, 36, 14, 37 };
-		int numero = 14;
-		
-		//Los arreglos iniciian en la posicion 0 
+		int[] array = { 35, 36, 14, 37, 22, 15, 35, 40, 22, 29, 35, 29, 22 };
+		int numero = 22;
+
+		// Los arreglos iniciian en la posicion 0
 		ejemplo.encuentraNumero(array, numero);
+		ejemplo.encuentraNumero2(array, numero);
+		ejemplo.encuentraOcurrencias(array, numero);
 	}
 }

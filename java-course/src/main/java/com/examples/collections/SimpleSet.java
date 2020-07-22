d36ad579d022b426e4c58cfd09ab83a354ffe001
Master_Boy <osorio.alvarez.adrian@gmail.com>
2020-07-22 14:25:29 -0500

    Add: ejercicio diasSemana usando Set

--- java-course/src/main/java/com/examples/collections/SimpleSet.java
+++ java-course/src/main/java/com/examples/collections/SimpleSet.java
@@ -8,65 +8,85 @@ import java.util.List;
 import java.util.Set;
 
 public class SimpleSet {
-	
-	public static void main( String[] args ) {
-		//creacion de sets
-		//Un set no permite valores duplicados
-		
-		//Se deben importar:
-		//java.util.HashSet
-		//java.util.Set
+
+	public static void main(String[] args) {
+		// creacion de sets
+		// Un set no permite valores duplicados
+
+		// Se deben importar:
+		// java.util.HashSet
+		// java.util.Set
 		Set<Integer> numbers = new HashSet<>();
-		
-		//Agregar un elemento
+
+		// Agregar un elemento
 		numbers.add(1);
 		numbers.add(2);
 		numbers.add(1);
-		System.out.println( numbers );
-		
-		//Tamanio del set
-		System.out.println( "Size: " + numbers.size() );
-		
-		//Verificar si existe un elemento
-		System.out.println( "Existe el elemento 3?: " + numbers.contains( 3 ) );
-		
-		//Eliminar un elemento
-		numbers.remove( 1 );
-		System.out.println( numbers );
-		
-		//Iterar elementos
-		System.out.println( "Iterar con for each" );
-		for(Integer i : numbers) {
-			System.out.println( i );
+		System.out.println(numbers);
+
+		// Tamanio del set
+		System.out.println("Size: " + numbers.size());
+
+		// Verificar si existe un elemento
+		System.out.println("Existe el elemento 3?: " + numbers.contains(3));
+
+		// Eliminar un elemento
+		numbers.remove(1);
+		System.out.println(numbers);
+
+		// Iterar elementos
+		System.out.println("Iterar con for each");
+		for (Integer i : numbers) {
+			System.out.println(i);
 		}
-		
-		//Con iterator
-		//Importar:
-		//java.util.Iterator
-		System.out.println( "Iterar con Iterator" );
+
+		// Con iterator
+		// Importar:
+		// java.util.Iterator
+		System.out.println("Iterar con Iterator");
 		Iterator<Integer> it = numbers.iterator();
-		while( it.hasNext() ) {
+		while (it.hasNext()) {
 			Integer i = it.next();
-			System.out.println( i );
+			System.out.println(i);
 		}
-		
-		//Convertir a List
+
+		// Convertir a List
 		List<Integer> list = new ArrayList<>();
 		list.addAll(numbers);
-		System.out.println( list );
-		
-		
-		//Convertir de List a Set
-		List<String> strings = Arrays.asList( "1", "2", "3", "1" );
+		System.out.println(list);
+
+		// Convertir de List a Set
+		List<String> strings = Arrays.asList("1", "2", "3", "1");
 		Set<String> sets = new HashSet<>();
 		sets.addAll(strings);
-		System.out.println( sets );
-		
-		//TODO: Adrian:
-		//Crear un Set de Strings que contenga los dias de la seman
-		//Iterar sobre el
-		//Despues convertir ese Set a List
-		
-		//
+		System.out.println(sets);
+
+		// TODO: Adrian:
+		// Crear un Set de Strings que contenga los dias de la semana
+		// Iterar sobre el
+		// Despues convertir ese Set a List
+		Set<String> diasSemana = new HashSet<>();
+		diasSemana.add("Lunes");
+		diasSemana.add("Martes");
+		diasSemana.add("Miercoles");
+		diasSemana.add("Jueves");
+		diasSemana.add("Viernes");
+		diasSemana.add("Sábado");
+		diasSemana.add("Domingo");
+
+		System.out.println("Días de la semana usando Set");
+
+		for (String s : diasSemana) {
+			System.out.println(s);
+		}
+
+		List<String> s = new ArrayList<>();
+		s.addAll(diasSemana);
+
+		System.out.println("Días de la semana con conversion a List");
+
+		for (String dias : s) {
+			System.out.println(dias);
+		}
 	}
 }
